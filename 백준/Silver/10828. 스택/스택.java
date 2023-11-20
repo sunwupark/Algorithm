696c9de726e16b1575206231a038775f0564b8e1
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2023-11-20 14:33:37 +0900

    [Silver IV] Title: 스택, Time: 392 ms, Memory: 18176 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -5,46 +5,43 @@ import java.util.Stack;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
+    Stack stack = new Stack();
 
-    Stack<Integer> stack = new Stack<Integer>();
-    int count = 0;
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    Integer N = Integer.parseInt(bufferedReader.readLine());
 
-    for(int i = 0; i < n; i++){
-      String input = br.readLine();
-      String[] strings = input.split(" ");
-      //System.out.println("strings[0] = " + strings[0]);
-      switch (strings[0]){
+    for(int i = 0; i < N; i++){
+      String[] strings = bufferedReader.readLine().split(" ");
+      switch(strings[0]){
         case "push":
-          stack.push(Integer.parseInt(strings[1]));
-          count++;
+          stack.push(strings[1]);
           break;
         case "top":
-          if(count == 0){
+          if(stack.isEmpty()){
             System.out.println(-1);
-          } else{
+          } else {
             System.out.println(stack.lastElement());
           }
           break;
         case "size":
-          System.out.println(count);
+          System.out.println(stack.size());
           break;
         case "empty":
-          if(count == 0){
+          if(stack.isEmpty()){
             System.out.println(1);
           } else{
             System.out.println(0);
           }
           break;
         case "pop":
-          if(stack.isEmpty()) {
+          if(stack.isEmpty()){
             System.out.println(-1);
           } else{
             System.out.println(stack.pop());
-            count--;          }
+          }
           break;
       }
     }
+
   }
 }
