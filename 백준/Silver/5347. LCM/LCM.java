f9e0a935f3b663f1275488ac793cafd5777e250d
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-01-24 17:42:56 +0900

    [Silver V] Title: LCM, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            long result, big, small;
+            if(a>b){
+                big = a;
+                small = b;
+            } else{
+                big = b;
+                small = a;
+            }
+
+            result = gcd(big,small);
+
+            System.out.println(big*small/result);
+        }
+    }
+
+    static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
