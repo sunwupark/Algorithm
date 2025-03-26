c93bea5e266ed1b2bc57523b8bddf925e1ec3363
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2025-03-26 13:27:24 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT i.animal_id as 'ANIMAL_ID', i.name as 'NAME'
+from animal_ins as i
+inner join animal_outs as o on i.animal_id = o.animal_id
+where i.datetime > o.datetime
+order by i.datetime
